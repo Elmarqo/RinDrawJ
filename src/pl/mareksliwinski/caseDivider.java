05c68c19d92262922c3535382539fb6f831c4aca
Marek Śliwiński <msliwinski@onet.pl>
2017-10-17 20:47:45 +0200

    Minor code rearrangement. outFirm class added.

--- src/pl/mareksliwinski/caseDivider.java
+++ src/pl/mareksliwinski/caseDivider.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 
 public class caseDivider{
 
-    private String [] outList = {"BMB", "DIAL", "KI"};
-
-    public double[] getPercentList() {
-        return percentList;
-    }
-
-    private double [] percentList = new double[3];
+    private double [] percentList = new double[outFirm.OUTNUMBER];
     private Scanner scanner = new Scanner(System.in);
 
+    /*Getter
+    double[] getPercentList() {
+        return percentList;
+    }*/
 
     void divider(){
+        outFirm outfirm = new outFirm();
         double percentageSum;
         do {
             percentageSum = 0;
-            for(int i = 0; i < percentList.length; i++){
-                System.out.print("Podaj udzial procentowy firmy " + outList[i] + " w przekazywanej transzy: \t");
+            for(int i = 0; i < outFirm.OUTNUMBER; i++){
+                System.out.print("Podaj udzial procentowy " + outfirm.getFirmName()[i] + " w przekazywanej transzy: \t");
                 percentList[i] = scanner.nextDouble();
                 percentageSum += percentList[i];
             }
+            if(percentageSum != 100)
+                System.out.println("\nSuma udzialow musi sie rownac 100%. Wprowadz dane ponownie:");
         }while (percentageSum != 100);
     }
 }
---
+++ src/pl/mareksliwinski/outFirm.java
@@ -0,0 +1,11 @@
+package pl.mareksliwinski;
+
+public class outFirm {
+
+    final static int OUTNUMBER= 3;
+    private String [] firmName = {"BMB", "DIALTONE", "KI"};
+
+    public String[] getFirmName() {
+        return firmName;
+    }
+}
