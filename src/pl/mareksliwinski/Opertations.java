40c14feb0d5e0be7d916084d37bca90bdd7cca52
Marek Śliwiński <msliwinski@onet.pl>
2017-11-11 10:28:22 +0100

    Work on Operations. Distinct is done.

--- src/pl/mareksliwinski/Opertations.java
+++ src/pl/mareksliwinski/Opertations.java
@@ -14,22 +14,28 @@ public class Opertations {
 
     public List<ArrayCreator> newOldTheSameId(List<ArrayCreator> newList, List<ArrayCreator> oldList) {
 
-        String pattern = "###,###.###";
-        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        String pattern = "###,###.##";
+        DecimalFormat dF = new DecimalFormat(pattern);
+
+        /*double amountSum =
+                newList.stream()
+                        .mapToDouble(ArrayCreator::getAmountDue)
+                        .sum();
+        System.out.println("suma: " + dF.format(amountSum));*/
 
         Set<String> tempIdCustomer = oldList.stream().parallel()
                 .map(ArrayCreator::getIdCustomer)
                 .sorted()
                 .collect(Collectors.toSet());
         System.out.println("\nLiczba unikalnych dluznikow w starej transzy: " +
-                decimalFormat.format(tempIdCustomer.size()));
+                dF.format(tempIdCustomer.size()));
 
         List<ArrayCreator> theSame =
                 newList.stream().parallel()
                         .filter(ac -> tempIdCustomer.contains(ac.getIdCustomer()))
                         .collect(Collectors.toList());
         System.out.println("Liczba dluznikow przekazanych w poprzednich transzach : " +
-                decimalFormat.format(theSame.size()));
+                dF.format(theSame.size()));
 
         return theSame;
     }
@@ -37,14 +43,19 @@ public class Opertations {
     List<ArrayCreator> DupNewListId(List<ArrayCreator> newList) {
 
         List<ArrayCreator> newListDuplicate = new ArrayList<>();
-        for (ArrayCreator elem : newList) {
+        /*for (ArrayCreator elem : newList) {
             int index = 0;
             for (ArrayCreator elem2 : newList)
                 if (elem.getIdCustomer().equals(elem2.getIdCustomer()))        //dodać drugi warunek na nr sprawy
                     index++;
             if (index > EXISTING_MORE_THEN_ONE_TIME)
                 newListDuplicate.add(elem);
-        }
+        }*/
+        double maxi = newList.stream()
+                .map(ArrayCreator::getAmountDue)
+                .reduce(-22221000.0, Double::max);
+        System.out.println("maxi: " + maxi);
+
         System.out.println(newListDuplicate.size());
         return newListDuplicate;
     }
